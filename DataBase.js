b2c9147b89935303ebad97a79a119214b8a2b7c7
AntoineXVI <104766441+AntoineXVI@users.noreply.github.com>
2023-12-20 17:48:56 +0100

    exitCollideDataBase

--- DataBase.js
+++ DataBase.js
@@ -1,26 +1,67 @@
-const buttonsDataBase = {
+export const buttonsDataBase = {
     machine1 : 
     {
         buttons :
         {
             testButton : {
-                clickAnimation : "c2743d71-8ac2-4259-bd77-96cb5f3bc02c",
-                clickCallBack : () => 
+                clickCallBack : async () => 
                 {
-
+                    await SDK3DVerse.engineAPI.playAnimationSequence("7526a81f-145f-436c-af18-0108b383a9aa", {seekOffset : 0});
                 }
             }
         }
     },
-    machine2 : {buttons : {startButton :"", stopButton:""}},
-    machine3 : {buttons : {startButton:"", stopButton:""}},
+
 };
 
-const collidDataBase = {
-    pad1 : {
-        onColidEvent : (emitterEntity) =>
-        {
-            setGlobalTransform({position : [0,0,0]});
+export const collideDataBase = {
+    TP_salle_chimie : {
+        pad1_triggeredBox : {
+            triggerCallBack : (emitterEntity) =>
+            {
+                console.log("rtest");
+                const transform =
+                {
+                    position : [5,0.1,-34],
+                    orientation : [0,0,0,1],
+                    scale : [1,1,1]
+                };
+                emitterEntity.setGlobalTransform(transform);
+            }
+        },
+        pad2_triggeredBox : {
+            triggerCallBack : (emitterEntity) =>
+            {
+                const transform =
+                {
+                    position : [0,0.1,0],
+                    orientation : [0,0,0,1],
+                    scale : [1,1,1]
+                };
+                emitterEntity.setGlobalTransform(transform);
+            }
+        },
+    },
+
+    machineTest : {
+        dvi3000 : {
+            triggerCallBack : (emitterEntity) =>
+            {
+                document.getElementById("myModal").style.display = "block";
+            }
         }
+        
+    }
+}
+
+export const exitCollideDataBase = {
+    machineTest : {
+        dvi3000 : {
+            triggerCallBack : (emitterEntity) =>
+            {
+                document.getElementById("myModal").style.display = "none";
+            }
+        }
+        
     }
 }

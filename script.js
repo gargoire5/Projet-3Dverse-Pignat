f1e3a139c26d5884061a273cfe867911edd5f188
AntoineXVI <104766441+AntoineXVI@users.noreply.github.com>
2023-12-20 17:48:17 +0100

    exittrigger

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+import * as Data from "./DataBase.js"   ;
+
 const canvas = document.getElementById('display-canvas');
 var selectedEntities; 
 var machineParent;
 var lookedButton;
+var lastHoverEntity;
 var animonoff = false;
 
 //-----------------------------------HightLight---------------------------------------------
@@ -10,12 +13,13 @@ canvas.addEventListener('mousemove', async (e) =>
     const {entity, pickedPosition, pickedNormal} = await SDK3DVerse.engineAPI.castScreenSpaceRay(e.clientX, e.clientY);
     if (!entity)
         return;
-
-    for (const [machine, machineInfo] of Object.entries(DataBase)) {
+    
+    for (const [machine, machineInfo] of Object.entries(Data.buttonsDataBase)) {
         for (const [buttons, buttonsList] of Object.entries(machineInfo)) {
             for (const [button, buttonInfo] of Object.entries(buttonsList)) {
                 if (button == entity.getName())
                 {
+                    lastHoverEntity = entity;
                     machineParent = machine;
                     lookedButton = entity;
                 }
@@ -36,35 +40,47 @@ canvas.addEventListener('mousemove', async (e) =>
     }
 
     //-----------------------------------onClickAction---------------------------------------------
-    canvas.addEventListener('mouseup', async () =>
-    {
-        
-    if (entity == lookedButton)
-    {
-        var animationUUID = buttonsDataBase[machineParent].buttons[lookedButton.getName()].clickAnimation;
-        if (animonoff == false)
-        {
-            SDK3DVerse.engineAPI.playAnimationSequence(animationUUID);
-            animonoff = true;
-        }
-            
-        else if (animonoff == true)
-        {
-            SDK3DVerse.engineAPI.stopAnimationSequence(animationUUID);
-            animonoff = false;
-        }
-            
-        
-    }
-    }, false);
+    
 
 
 }, false);
 
-const Teleport_player = (emitterEntity,triggerEntity) =>
+canvas.addEventListener('mouseup', async (e) =>
 {
-  triggerEntity = triggerEntity.getParent().getName();
-  collidDataBase[triggerEntity].onColidEvent()
+    const {entity} = await SDK3DVerse.engineAPI.castScreenSpaceRay(e.clientX, e.clientY);
+    if (entity == lookedButton)
+        Data.buttonsDataBase[machineParent].buttons[lookedButton.getName()].clickCallBack();
 
+}, false);
+
+// TELEPORT
+
+const collideTrigger = (emitterEntity,triggerEntity) =>
+{
+    for (const [collideGroup, inGroupList] of Object.entries(Data.collideDataBase)) 
+    {
+        for (const [collideTriggers, collideTriggersInfo] of Object.entries(inGroupList)) 
+        {
+            if (triggerEntity.getName() == collideTriggers ) 
+            {
+                Data.collideDataBase[collideGroup][collideTriggers].triggerCallBack(emitterEntity);
+            }
+        }
+    }
 }
-SDK3DVerse.engineAPI.onEnterTrigger(Teleport_player);
+SDK3DVerse.engineAPI.onEnterTrigger(collideTrigger);
+
+const exitCollideTrigger = (emitterEntity,triggerEntity) =>
+{
+    for (const [collideGroup, inGroupList] of Object.entries(Data.exitCollideDataBase)) 
+    {
+        for (const [collideTriggers, collideTriggersInfo] of Object.entries(inGroupList)) 
+        {
+            if (triggerEntity.getName() == collideTriggers ) 
+            {
+                Data.exitCollideDataBase[collideGroup][collideTriggers].triggerCallBack(emitterEntity);
+            }
+        }
+    }
+}
+SDK3DVerse.engineAPI.onExitTrigger(exitCollideTrigger);
